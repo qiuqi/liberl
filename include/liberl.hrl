2cf4cd57230fef9e11ddaae2d880fdbb3c1b1f2d
qiuqi <qiu.qiq@gmail.com>
2016-08-02 20:43:44 +0800

    include for all projects

--- include/liberl.hrl
+++ include/liberl.hrl
@@ -1,7 +1,7 @@
 -ifndef(LIBERL_INCLUDE_HRL).
 -define(LIBERL_INCLUDE_HRL, ok).
 
--define(B(F), lager:log(info, "tag", "~p:~p ~p", [?MODULE, ?LINE, F])).
+-define(B(F), lager:log(info, "dbg", "~p:~p ~p", [?MODULE, ?LINE, F])).
 -define(U(F), unicode:characters_to_binary(F)).
 
 -define(GETVALUE(K, L), proplists:get_value(K, L)).
@@ -23,5 +23,12 @@
 -define(HTOB(F), missing_hex:hexstr_to_bin(F)).
 -define(BTOH(F), missing_hex:bin_to_hexstr(F)).
 -define(CHECK(F, R), ancode_utils:checkError(F, Reason)).
+-define(B64(F), base64:encode(F)).
+-define(BIN(F), ancode_utils:bin(F)).
+
+-define(HEADER(Key, Req), cowboy_req:header(Key, Req)).
+-define(HEADERS(Key, Req), binary_to_list(cowboy_req:header(Key, Req))).
+-define(BODY(Req), cowboy_req:read_body(Req, [])).
+
 
 -endif.
