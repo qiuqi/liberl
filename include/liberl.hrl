9c8c9da1dadab39c6b24dbe91b4ce8100319b3a4
qiuqi <qiu.qiq@gmail.com>
2016-08-29 17:44:33 +0800

    cowboy2

--- include/liberl.hrl
+++ include/liberl.hrl
@@ -7,12 +7,12 @@
 
 -define(U(F), unicode:characters_to_binary(F)).
 -define(S(F), binary_to_list(F)).
--define(HTTP_OK(Req, Opts), j:jsonOk(Req,Opts)).
--define(HTTP_OK_TIME(Req, Time, Opts), j:jsonOkTime(Req, Time, Opts)).
--define(HTTP_FAILED(Req, Opts), j:jsonFailed(Req, Opts)).
+-define(HTTP_OK(Req), j:jsonOk(Req)).
+-define(HTTP_OK_TIME(Req, Time), j:jsonOkTime(Req, Time)).
+-define(HTTP_FAILED(Req), j:jsonFailed(Req)).
 -define(HEADER(Key, Req), cowboy_req:header(Key, Req)).
 -define(HEADERS(Key, Req), binary_to_list(cowboy_req:header(Key, Req))).
--define(BODY(Req), cowboy_req:read_body(Req, [])).
+-define(BODY(Req), cowboy_req:read_body(Req, #{length => infinity})).
 
 -define(JSON(F), binary_to_list(list_to_binary(mochijson2:encode(F)))).
 -define(JSON_DECODE(F), mochijson2:decode(F)).
