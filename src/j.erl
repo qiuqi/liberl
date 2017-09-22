8bfce9005546d0b48f67b8e9095603c0100986be
qiuqi <qiu.qiq@gmail.com>
2017-09-22 11:42:36 +0800

    profile eatch request performance

--- src/j.erl
+++ src/j.erl
@@ -2,10 +2,13 @@
 -include_lib("liberl/include/liberl.hrl").
 -export([json/2]).
 -export([jsonOk/1]).
+-export([jsonOk/2]).
 -export([jsonOkTime/2]).
 -export([jsonFailed/1]).
+-export([jsonFailed/2]).
 -export([keepalive/1]).
 -export([http_show_json/2]).
+-export([http_show_json/3]).
 -export([http_show_json_s/2]).
 -export([http_show_xml_s/2]).
 -export([http_show_xml/2]).
@@ -38,8 +41,23 @@ http_show_json_s(Req, Json)->
 %%	?B([http_show_json_s, All]),
     cowboy_req:stream_body(All++"\r\n", nofin, Req).
 
+http_show_json(Req, Msg, Opts)->
+    json(Req, [{"time", ?U(qqtime:longtime())}|Msg], Opts).
+
 http_show_json(Req, Msg)->
     json(Req, [{"time", ?U(qqtime:longtime())}|Msg]).
+
+json(Req, JsonString, Opts)->
+    Path = cowboy_req:path(Req),
+    Device = cowboy_req:header(<<"device">>, Req),
+    [TimeOld] = Opts,
+    Cost = qqtime:longtime_int()-TimeOld,
+%%    Json = mochijson2:encode(JsonString),
+    Json = mochijson2:encode([{"cost", ?U(integer_to_list(Cost))}|JsonString]),
+    ?B([httptag, Path, Cost]),
+    cowboy_req:reply(200, #{<<"content-type">> => <<"text/json; charset=utf-8">>}, Json, Req),
+    ?B([httptag, Device, Path, qqtime:longtime_int()-TimeOld]),
+    {ok, Req, #{}}.
 
 json(Req, JsonString)->
     Json = mochijson2:encode(JsonString),
@@ -64,6 +82,24 @@ jsonOk(Req)->
              {"value", true}
             ]},
     json(Req, Json).
+
+jsonOk(Req, Opts)->
+    Json = [
+             {"type", ?U("Accept")},
+             {"time", ?U(qqtime:longtime())},
+             {"value", true}
+            ],
+    json(Req, Json, Opts).
+
+jsonFailed(Req, Opts)->
+    Json = {struct,
+     [
+      {"type", ?U("Accept")},
+      {"time", ?U(qqtime:longtime())},
+      {"value", false}
+     ]},
+    json(Req, Json, Opts).
+
 
 jsonFailed(Req)->
     Json = {struct,
